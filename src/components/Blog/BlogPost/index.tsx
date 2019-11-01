b34e2966b44b578a411faa3d7fd797255c6eff1e
Carrie Forde <cforde@bills.com>
2019-10-31 18:30:39 -0700

    Updates blog component

--- src/components/Blog/BlogPost/index.tsx
+++ src/components/Blog/BlogPost/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Post } from '../../../interfaces/post.interface';
+import { Link } from 'gatsby';
 import Category from '../../Category';
 import Tags from '../../Tags';
+import TimeStamp from '../../TimeStamp';
 
-import BlogPostHeader from './BlogPostHeader';
+import styles from './blog-post.module.css';
 
 const BlogPost = (data: Post) => {
   const { excerpt, frontmatter, timeToRead, fields } = data.node,
@@ -11,15 +13,28 @@ const BlogPost = (data: Post) => {
 
   return (
     <article>
-      <BlogPostHeader
-        slug={fields.slug}
-        title={title}
-        date={date}
-        timeToRead={timeToRead}
-      />
+      <header>
+        <Link className={styles.titleLink} to={fields.slug}>
+          <h2
+            className={styles.title}
+            dangerouslySetInnerHTML={{ __html: title }}
+          />
+        </Link>
+        {(date || timeToRead) && (
+          <p className={styles.meta}>
+            {date && <TimeStamp date={date} />}
+            {timeToRead && (
+              <>
+                <span className={styles.separator}>&#124;</span>
+                <span>{timeToRead} minute read</span>
+              </>
+            )}
+          </p>
+        )}
+      </header>
       <div dangerouslySetInnerHTML={{ __html: description || excerpt }} />
       {(category || tags) && (
-        <footer>
+        <footer className={styles.meta}>
           {category && (
             <div>
               Posted in: <Category category={category} />
